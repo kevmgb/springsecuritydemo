26f60bbcbcac4b92f070ebaf3f7c5aa9d9acad2c
kevmgb <kithinjimkevin@gmail.com>
2022-07-25 20:36:51 +0300

    Set up authentication and login endpoint

--- src/main/java/com/example/springsecuritydemo/configs/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.springsecuritydemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,6 +31,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found")));
     }
 
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
